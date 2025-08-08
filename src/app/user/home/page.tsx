956f239d558ca22e7f4a134eac1a13fa830047f2
tphuong3108 <phuongbeo3108203@gmail.com>
2025-08-08 09:28:43 +0700

    xóa file blur

--- src/app/user/home/page.tsx
+++ src/app/user/home/page.tsx
@@ -2,7 +2,6 @@ import SearchBox from '@/components/ui/SearchBox';
 import HomeBanner from './HomeBanner';
 import ServiceSection from './ServiceSection';
 import HotDestinations from './HotDestinations';
-import BlurBackground from '@/components/ui/BlurBackground';
 import HotSearchSection from './HotSearchSection';
 import PopularPostsSection from './PopularPostsSection';
 import HotBloggerSection from './HotBloggerSection';
@@ -14,7 +13,12 @@ import QNASection from './QNASection';
 export default function UserHomePage() {
   return (
     <div className="">
-      <BlurBackground />
+    <div className="absolute w-[500px] h-[450px] bg-[var(--secondary)] opacity-50 blur-[250px] pointer-events-none" style={{ top: "400px", left: "-420px" }} />
+    <div className="absolute w-[500] h-[550px] bg-[var(--primary)] opacity-50 blur-[250px] pointer-events-none" style={{ top: "770px", left: "1470px" }} />
+    <div className="absolute w-[400px] h-[300px] bg-[var(--primary)] opacity-50 blur-[250px] pointer-events-none" style={{ top: "1350px", left: "-300px" }} />
+    <div className="absolute w-[500px] h-[450px] bg-[var(--secondary)] opacity-50 blur-[250px] pointer-events-none" style={{ top: "2050px", left: "1470px" }} />
+    <div className="absolute w-[400px] h-[300px] bg-[var(--primary)] opacity-50 blur-[250px] pointer-events-none" style={{ top: "2980px", left: "-150px" }} />
+    <div className="absolute w-[500px] h-[550px] bg-[var(--secondary)] opacity-50 blur-[250px] pointer-events-none" style={{ top: "4750px", left: "1470px" }} />
       <HomeBanner />
       <SearchBox />
       <ServiceSection />
