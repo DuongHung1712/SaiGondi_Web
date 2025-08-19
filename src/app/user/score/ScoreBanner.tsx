d6c5f26486e242d98a78f4c0a2ff436837f27be7
tphuong3108 <phuongbeo3108203@gmail.com>
2025-08-19 17:00:45 +0700

    em chưa responsive đc mobile L và 1440px

--- src/app/user/score/ScoreBanner.tsx
+++ src/app/user/score/ScoreBanner.tsx
@@ -2,18 +2,50 @@
 import Image from 'next/image';
 
 const milestones = [
-  { title: 'TÂN KHÁM PHÁ', points: 0, icon: '/icon4.svg', color: '#5161EB', top: '82%', left: '28%' },
-  { title: 'KHÁM PHÁ VIÊN', points: 100, icon: '/icon5.svg', color: '#F96651', top: '36%', left: '45.5%' },
-  { title: 'THỔ ĐỊA', points: 300, icon: '/icon6.svg', color: '#02714D', top: '22%', left: '71.5%' },
-  { title: 'NHÀ THÁM HIỂM', points: 600, icon: '/icon7.svg', color: '#7829EC', top: '-25%', right: '8.9%' },
+  {
+    title: 'TÂN KHÁM PHÁ',
+    points: 0,
+    icon: '/icon4.svg',
+    color: '#5161EB',
+    className: "absolute top-[118%] left-[28%] ml:top-[100%] ml:left-[30%] md:top-[102%] md:left-[26%] lg:top-[98%] lg:left-[28%] xl:top-[92%] xl:left-[28%] 1.5xl:top-[152%] 1.5xl:left-[28%]"
+  },
+  {
+    title: 'KHÁM PHÁ VIÊN',
+    points: 100,
+    icon: '/icon5.svg',
+    color: '#F96651',
+    className: "absolute top-[95%] left-[44%] ml:top-[80%] ml:left-[40%] md:top-[70%] md:left-[44%] lg:top-[68%] lg:left-[46%] xl:top-[46%] xl:left-[45.5%] 1.5xl:top-[89%] 1.5xl:left-[30%]"
+  },
+  {
+    title: 'THỔ ĐỊA',
+    points: 300,
+    icon: '/icon6.svg',
+    color: '#02714D',
+    className: "absolute top-[84%] left-[71.5%] ml:top-[80%] ml:left-[60%] md:top-[61%] md:left-[70%] lg:top-[59%] lg:left-[72%] xl:top-[32%] xl:left-[71.5%] 1.5xl:top-[92%] 1.5xl:left-[55%]"
+  },
+  {
+    title: 'NHÀ THÁM HIỂM',
+    points: 600,
+    icon: '/icon7.svg',
+    color: '#7829EC',
+    className: "absolute top-[49%] right-[3%] ml:top-[45%] ml:right-[5%] md:top-[16%] md:right-[8%] lg:top-[19%] lg:left-[89%] xl:top-[-25%] xl:right-[8.9%]"
+  },
 ];
 
 export default function ScoreBanner() {
   return (
     <div className="relative w-full pt-20 pb-10 overflow-hidden shadow-lg">
-      <div className="absolute bottom-0 w-full z-0 translate-x-[120px] lg:translate-x-[10px]">
-          <Image src="/Vector_3.svg" alt="đường kẻ" width={1555} height={100} />
-        <div className="absolute bottom-0 w-full top-1 z-0 translate-x-[120px] sm:translate-x-[60px] md:translate-x-[80px] lg:translate-x-[135px]">
+      <div className="absolute bottom-0 w-full z-0 translate-x-[5px] sm:translate-x-[40px] md:translate-x-[-1px] xl:translate-x-[5px] lg:translate-x-[10px]">
+        <Image src="/Vector_3.svg" alt="đường kẻ" width={1555} height={100} />
+        <div className="absolute bottom-0 w-full top-2 z-0
+          translate-x-[8px] translate-y-[-14px]
+          sm:translate-x-[20px] sm:translate-y-[-35px]
+          md:translate-x-[55px] md:translate-y-[-12px] md:w-[720px]
+          lg:translate-x-[85px] lg:translate-y-[-12px] lg:w-[950px]
+          xl:translate-x-[135px] xl:translate-y-[-4px] xl:w-[1555px]
+          1.5xl:translate-x-[145px] 1.5xl:translate-y-[-15px] 1.5xl:w-[1750px]
+          "
+        >
           <Image src="/blur.svg" alt="Blur effect" width={1388} height={100} />
         </div>
       </div>
@@ -23,75 +55,29 @@ export default function ScoreBanner() {
           CHINH PHỤC DANH HIỆU <br className="hidden sm:block" /> CHO BẢN THÂN
         </h2>
 
-        <div className="relative w-full h-[280px] sm:h-[380px] md:h-[450px] lg:h-[350px]">
-          {milestones.map((m, i) => {
-            const isSpecial = m.title === 'NHÀ THÁM HIỂM';
-            return (
-              <div
-                key={i}
-                className="absolute z-20"
-                style={{
-                  top: m.top,
-                  ...(m.left && { left: m.left }),
-                  ...(m.right && { right: m.right }),
-                  transform: 'translate(-50%, -50%)',
-                }}
-              >
-                {isSpecial ? (
-                  <>
-                    <div
-                      className="absolute text-left whitespace-nowrap -translate-y-1/2"
-                      style={{
-                        top: '-30%',
-                        left: '-160px',
-                      }}
-                    >
-                      <p
-                        className="text-lg sm:text-xl lg:text-2xl font-bold"
-                        style={{ color: m.color }}
-                      >
-                        {m.title}
-                      </p>
-                      <span className="text-sm sm:text-base text-gray-500">
-                        {m.points} ĐIỂM
-                      </span>
-                    </div>
+        <div className="relative w-full h-[140px] sm:h-[380px] md:h-[250px] lg:h-[350px]">
+          {milestones.map((m, i) => (
+            <div
+              key={i}
+              className={`${m.className} z-20`}
+              style={{ transform: 'translate(-50%, -50%)' }}
+            >
+              <div className="relative">
+                <div className="absolute -top-[42px] translate-x-[-68%] sm:-top-[52px] md:-top-[60px] lg:-top-[74px] lg:-translate-x-[78%] left-1/2 -translate-x-1/2 flex flex-col items-start whitespace-nowrap">
+                  <p className="text-[12px] sm:text-sm lg:text-2xl font-bold leading-tight" style={{ color: m.color }}>
+                    {m.title}
+                  </p>
+                  <span className="text-[11px] sm:text-sm text-gray-500 mt-0.5 sm:mt-1 lg:text-xl ">
+                    {m.points} ĐIỂM
+                  </span>
+                </div>
 
-                    <div className="flex justify-center">
-                      <Image
-                        src={m.icon}
-                        alt={m.title}
-                        width={30}
-                        height={30}
-                        className="sm:w-[35px] sm:h-[35px] lg:w-[40px] lg:h-[40px]"
-                      />
-                    </div>
-                  </>
-                ) : (
-                  <div className="flex flex-col items-start">
-                    <p
-                      className="text-lg sm:text-xl lg:text-2xl font-bold"
-                      style={{ color: m.color }}
-                    >
-                      {m.title}
-                    </p>
-                    <span className="text-sm sm:text-base text-gray-500 mb-2 sm:mb-4">
-                      {m.points} ĐIỂM
-                    </span>
-                    <div className="self-center">
-                      <Image
-                        src={m.icon}
-                        alt={m.title}
-                        width={30}
-                        height={30}
-                        className="sm:w-[35px] sm:h-[35px] lg:w-[40px] lg:h-[40px]"
-                      />
-                    </div>
-                  </div>
-                )}
+                <div className="relative w-[27px] h-[27px] sm:w-[24px] sm:h-[24px] md:w-[36px] md:h-[36px] lg:w-[50px] lg:h-[50px] 1.5xl:w-[70px] 1.5xl:h-[70px]">
+                  <Image src={m.icon} alt={m.title} fill className="object-contain" />
+                </div>
               </div>
-            );
-          })}
+            </div>
+          ))}
         </div>
       </div>
     </div>
